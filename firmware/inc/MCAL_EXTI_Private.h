0eaf57ae4f6077cfd9a0555b55240bdc27cf69e7
Khaled Abdulaziz <khaled.ibrahim.user@gmail.com>
2023-06-28 02:14:09 +0300

    Added comments to the MCAL_EXTI_Private.h file.
    Comments to the SYSCFG_Reg_t and EXTI_Reg_t structures, explaining the purpose of each member.
    Comments to the SYSCFG and EXTI macros, explaining the purpose of each macro.
    @brief tags to each struct member, telling Doxygen to generate a brief description of the member.

--- firmware/inc/MCAL_EXTI_Private.h
+++ firmware/inc/MCAL_EXTI_Private.h
@@ -1,28 +1,97 @@
+/**
+ * @file MCAL_EXTI_Private.h
+ * @brief This file contains the private implementation of the EXTI peripheral.
+ *
+ * @author Ahmed Abu Raya
+ * @date 2023-06-20
+ */
+
 #ifndef MCAL_EXTI_PRIVATE_H_
 #define MCAL_EXTI_PRIVATE_H_
 
+/**
+ * @headerfile "MCAL_EXTI_Private.h"
+ * @brief This header file contains the private implementation of the EXTI peripheral.
+ */
 #include "MCAL_GPIO_Private.h"
 
+/**
+ * @struct	SYSCFG_Reg_t
+ * @brief This structure defines the SYSCFG registers.
+ */
 typedef struct {
-	vu32 MEMRMP;       /*!< SYSCFG memory remap register,                      Address offset: 0x00      */
-	vu32 PMC;          /*!< SYSCFG peripheral mode configuration register,     Address offset: 0x04      */
-	vu32 EXTICR[4];    /*!< SYSCFG external interrupt configuration registers, Address offset: 0x08-0x14 */
-	u32 RESERVED[2];  /*!< Reserved, 0x18-0x1C                                                          */
-	vu32 CMPCR;        /*!< SYSCFG Compensation cell control register,         Address offset: 0x20      */
+	/**
+	 * @brief This register is used to remap the memory.
+	 */
+	vu32 MEMRMP;
+
+	/**
+	 * @brief This register is used to configure the peripheral mode.
+	 */
+	vu32 PMC;
+
+	/**
+	 * @brief This array of registers is used to configure the external interrupt lines.
+	 */
+	vu32 EXTICR[4];
+
+	/**
+	 * @brief Reserved.
+	 */
+	u32 RESERVED[2];
+
+	/**
+	 * @brief This register is used to configure the compensation cell.
+	 */
+	vu32 CMPCR;
 } SYSCFG_Reg_t;
 
+/**
+ * @struct EXTI_Reg_t
+ * @brief This structure defines the EXTI registers.
+ */
 typedef struct {
 
+	/**
+	 * @brief This register is used to mask the interrupts.
+	 */
 	vu32 IMR;
+
+	/**
+	 * @brief This register is used to enable the interrupts.
+	 */
 	vu32 EMR;
+
+	/**
+	 * @brief This register is used to set the rising trigger.
+	 */
 	vu32 RTSR;
+
+	/**
+	 * @brief This register is used to set the falling trigger.
+	 */
 	vu32 FTSR;
+
+	/**
+	 * @brief This register is used to generate software interrupt.
+	 */
 	vu32 SWIER;
+
+	/**
+	 * @brief This register is used to store the pending requests.
+	 */
 	vu32 PR;
 
 } EXTI_Reg_t;
 
+/**
+ * @brief This macro is used to get the base address of the SYSCFG peripheral.
+ */
 #define SYSCFG            ((SYSCFG_Reg_t *)SYSCFG_BASE)
+
+/**
+ * @brief This macro is used to get the base address of the EXTI peripheral.
+ */
 #define EXTI              ((EXTI_Reg_t *)EXTI_BASE)
 
 #endif /* MCAL_EXTI_PRIVATE_H_ */
