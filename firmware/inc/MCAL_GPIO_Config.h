3334f61c90df9c84249c41dc20a98b345b63b3aa
Khaled Abdulaziz <khaled.ibrahim.user@gmail.com>
2023-06-28 02:34:59 +0300

    Added comments to the `MCAL_GPIO_Config.h` file.
    * Added comments to the file.
    * Added the `@headerfile` tag to the `#include` statement.

--- firmware/inc/MCAL_GPIO_Config.h
+++ firmware/inc/MCAL_GPIO_Config.h
@@ -1,6 +1,18 @@
+/**
+ * @file MCAL_GPIO_Config.h
+ * @brief This file contains the configuration for the GPIO peripheral.
+ *
+ * @author Ahmed Abu Raya
+ * @date 2023-06-20
+ */
+
 #ifndef MCAL_GPIO_CONFIG_H_
 #define MCAL_GPIO_CONFIG_H_
 
+/**
+ * @headerfile "MCAL_GPIO_Interface.h"
+ * @brief This header file contains the interface for the GPIO peripheral.
+ */
 #include "MCAL_GPIO_Interface.h"
 
 #endif /* MCAL_GPIO_CONFIG_H_ */
