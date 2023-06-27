58a8d14f896907d52e7ae39a03b6b781415b6fac
Khaled Abdulaziz <khaled.ibrahim.user@gmail.com>
2023-06-28 01:57:35 +0300

    Added Doxygen comments Added Doxygen comments to the top of the file, including the author, date, and brief description of the file. Added Doxygen comments to the #include statements, specifying the header files that are included. Added Doxygen comments to the NUMOF_CONFIG_EXTI_SOURCES macro, specifying the name of the macro and the value of the macro. This commit improves the documentation for the MCAL_EXTI_Config.h file by making it more readable and understandable.

--- firmware/inc/MCAL_EXTI_Config.h
+++ firmware/inc/MCAL_EXTI_Config.h
@@ -1,10 +1,36 @@
+/**
+ * @file MCAL_EXTI_Config.h
+ * @brief This file contains the configuration for the EXTI peripheral.
+ *
+ * @author Ahmed Abu Raya
+ * @date 2023-06-20
+ */
+
 #ifndef MCAL_EXTI_CONFIG_H_
 #define MCAL_EXTI_CONFIG_H_
 
+/**
+ * @headerfile "MCAL_GPIO_Interface.h"
+ * @brief This header file contains the interface for the GPIO peripheral.
+ */
 #include "MCAL_GPIO_Interface.h"
+
+/**
+ * @headerfile "MCAL_EXTI_Interface.h"
+ * @brief This header file contains the interface for the EXTI peripheral.
+ */
 #include "MCAL_EXTI_Interface.h"
+
+/**
+ * @headerfile "STM32F4xx_Device_Header.h"
+ * @brief This header file contains the definitions for the STM32F4xx device.
+ */
 #include "STM32F4xx_Device_Header.h"
 
+/**
+ * @def	NUMOF_CONFIG_EXTI_SOURCES
+ * @brief The number of configurable EXTI sources.
+ */
 #define NUMOF_CONFIG_EXTI_SOURCES            48
 
 #endif /* MCAL_EXTI_CONFIG_H_ */
