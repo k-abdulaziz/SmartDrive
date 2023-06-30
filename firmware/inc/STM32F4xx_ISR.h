82238e0dd7e3baa569faea5c05734f8563083ca7
Khaled Abdulaziz <khaled.ibrahim.user@gmail.com>
2023-07-01 02:27:51 +0300

    Add Doxygen comments for improved documentation

--- firmware/inc/STM32F4xx_ISR.h
+++ firmware/inc/STM32F4xx_ISR.h
@@ -1,3 +1,10 @@
+/**
+ * @file _STM32F4xx_ISR_H
+ * @brief This file contains the ISR for STM32F4XX.
+ *
+ * @author Ahmed Abu Raya
+ * @date 2023-06-20
+ */
 
 #ifndef _STM32F4xx_ISR_H
 #define _STM32F4xx_ISR_H
