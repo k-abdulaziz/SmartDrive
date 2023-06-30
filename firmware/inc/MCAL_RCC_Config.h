4080faf7927fc851281da73d0045366677b5099f
Khaled Abdulaziz <khaled.ibrahim.user@gmail.com>
2023-06-30 19:00:17 +0300

    Add Doxygen comments for improved documentation

--- firmware/inc/MCAL_RCC_Config.h
+++ firmware/inc/MCAL_RCC_Config.h
@@ -1,7 +1,24 @@
+/**
+ * @file MCAL_RCC_Config.h
+ * @brief This file contains the configuration for the RCC peripheral.
+ *
+ * @author Ahmed Abu Raya
+ * @date 2023-06-20
+ */
+
 #ifndef MCAL_RCC_CONFIG_H_
 #define MCAL_RCC_CONFIG_H_
 
+/**
+ * @headerfile "MCAL_RCC_Interface.h"
+ * @brief Include the header file for the RCC interface.
+ */
 #include "MCAL_RCC_Interface.h"
+
+/**
+ * @headerfile "STM32F4xx_Device_Header.h"
+ * @brief Include the header file for the STM32F4xx device.
+ */
 #include "STM32F4xx_Device_Header.h"
 
 #endif /* MCAL_RCC_CONFIG_H_ */
