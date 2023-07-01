d2e1df04b980386883bf8092e75295e5606d4fd4
Khaled Abdulaziz <khaled.ibrahim.user@gmail.com>
2023-07-01 03:17:01 +0300

    Add IR_Private.h with doxygen comments

---
+++ firmware/ECUAL/IR_Sensor/IR_Private.h
@@ -0,0 +1,14 @@
+/**
+ * @file IR_Private.h
+ * @brief Private header file for the IR module.
+ *
+ * This file is the private header file for the IR module.
+ * It does not contain any public function declarations or macros.
+ */
+
+#ifndef __IR_PRIVATE_H__
+#define __IR_PRIVATE_H__
+
+
+
+#endif /*__IR_PRIVATE_H__*/
